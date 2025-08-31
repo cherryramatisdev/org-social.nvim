a2d26d074bf2396b2083b3b06cd6731d6f0868d4
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-08-31 18:44:32 -0300

    feat(view): Adjust timeline view function to sort posts
    Also showing less information initially

--- lua/org-social/view.lua
+++ lua/org-social/view.lua
@@ -2,35 +2,49 @@ local M = {}
 
 function M.render_timeline(parsed_feeds)
     local lines = {}
+    local all_posts = {}
 
     for username, feed in pairs(parsed_feeds) do
-        table.insert(lines, string.format("* %s - %s", feed.metadata.TITLE or "", feed.metadata.NICK or username))
-        table.insert(lines, string.format(":PROPERTIES:"))
-        table.insert(lines, string.format(":DESCRIPTION: %s", feed.metadata.DESCRIPTION or ""))
-        table.insert(lines, string.format(":LINK: %s", feed.metadata.LINK or ""))
-        table.insert(lines, string.format(":CONTACT: %s", feed.metadata.CONTACT or ""))
-        if feed.metadata.AVATAR then
-            table.insert(lines, string.format(":AVATAR: %s", feed.metadata.AVATAR))
+        for _, post in ipairs(feed.posts) do
+            if post.content and #post.content > 0 then
+                local post_copy = vim.deepcopy(post)
+                post_copy.author = feed.metadata
+                post_copy.author.username = username
+                table.insert(all_posts, post_copy)
+            end
         end
-        table.insert(lines, string.format(":ENDPROPERTIES:"))
-        table.insert(lines, string.format("** Follows"))
+    end
 
-        for _, follow in ipairs(feed.follows) do
-            table.insert(lines, string.format("- [[%s][%s]]", follow.url, follow.name))
+    table.sort(all_posts, function(a, b)
+        return a.timestamp > b.timestamp
+    end)
+
+    table.insert(lines, '#+TITLE: Org Social Timeline')
+    table.insert(lines, '')
+    table.insert(lines, "* Timeline")
+
+    for _, post in ipairs(all_posts) do
+        table.insert(lines, string.format("** %s",
+            post.author.username or post.author.NICK
+        ))
+
+        table.insert(lines, "")
+        local content_lines = vim.split(post.content, '\n')
+        for _, line in ipairs(content_lines) do
+            table.insert(lines, line)
         end
-
-        table.insert(lines, "") -- Add empty line between feeds
     end
 
     local temp_file = vim.fn.tempname() .. '.org'
     vim.fn.writefile(lines, temp_file)
     vim.cmd.vsplit(temp_file)
+
     local buf = vim.api.nvim_get_current_buf()
 
     vim.api.nvim_set_option_value('modified', false, { scope = 'local', buf = buf })
     vim.api.nvim_set_option_value('bufhidden', 'delete', { scope = 'local', buf = buf })
 
-    vim.api.nvim_create_autocmd({'BufDelete', 'BufWipeout'}, {
+    vim.api.nvim_create_autocmd({ 'BufDelete', 'BufWipeout' }, {
         buffer = buf,
         callback = function()
             if vim.fn.filereadable(temp_file) == 1 then
